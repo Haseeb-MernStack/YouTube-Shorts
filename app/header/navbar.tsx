0005ead721d97aa944c11fef7f7fe61197d42cd1
Haseeb MernStack <seebpubg@gmail.com>
2025-05-02 20:24:43 +0500

    Updated

--- app/header/navbar.tsx
+++ app/header/navbar.tsx
@@ -2,13 +2,16 @@ import React from 'react'
 import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
 import { Plus } from 'lucide-react'
+import Image from 'next/image';
+import short from "@/public/shorts.webp";
 
 const navbar = () => {
     return (
         <div className='h-14 flex items-center justify-between'>
             {/* logo text */}
-            <div>
-                <h1 className='text-xl font-bold'>YT<span className='text-red-500'>Shorts</span></h1>
+            <div className='flex items-center gap-x-2'>
+                <h1 className='text-xl font-bold'>YT<span className='text-red-500'>Sho<span className='text-white'>r</span>ts</span></h1>
+                <Image src={short} alt='logo' height={12} width={12} />
             </div>
             {/* Search input field */}
             <div>
